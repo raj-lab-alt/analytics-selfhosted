373f0e0a8244c9ae06ad0bece00dc7af640e0c0a
raj-lab-alt <rajah.shili.tn@gmail.com>
2026-06-11 15:15:05 +0100

    Fix CORS preflight: add Access-Control-Allow-Methods + return 204

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const wss = new WebSocketServer({ server });
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
 app.use(express.json({ limit: '50kb' }));
